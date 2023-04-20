82789bf09a03c87f4af4092f16ac1904d733dca6
jacqeinz <jacquicv.19@gmail.com>
2023-04-20 12:48:30 -0600

    index flask

---
+++ Stage 3/index.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, abort
+
+app = Flask(__name__)
+
+@app.route("/index.html")
+# @app.route("/index")
+def index():
+    return redirect('/index.html')
+
+@app.route('/milkshakes.html')
+def milkshakes():
+    return redirect('/milkshakes.html')
+
+@app.route('/sundaes.html')
+def sundaes():
+    return redirect('/sundaes.html')
+
+@app.route('/snowcones.html')
+def snowcones():
+    return redirect('/snowcones.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
