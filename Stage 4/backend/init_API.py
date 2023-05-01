8d0bc4c7021d4b11a6c46dfe4ecaf99a46d135b5
jacqeinz <jacquicv.19@gmail.com>
2023-04-30 21:45:41 -0600

    added snowcone endpoint

--- Stage 4/backend/init_API.py
+++ Stage 4/backend/init_API.py
@@ -6,13 +6,14 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from sqlalchemy import Column, ForeignKey, Integer, Unicode
 from sqlalchemy.orm import relationship
+from socket import gethostname
 
 import json
 # tells flask where to put static files
 # COLLECT_STATIC_ROOT = os.path.dirname(__file__) + '/static'
 # COLLECT_STORAGE = 'flask_collect.storage.file'
 
-#SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 #json_url = os.path.join(SITE_ROOT, "static", "defaultData.json")
 #data = json.load(open(json_url))
 # create the app
@@ -32,36 +33,36 @@ app.app_context().push()
 class sundaes (db.Model):
     id = db.Column('sundae_id', db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    hasFlavors = db.Column(db.String(10))
+    has_flavors = db.Column(db.String(10))
     description = db.Column(db.String(50))
-    Price = db.Column(db.String(50))
+    price = db.Column(db.String(50))
     data = db.Column(db.LargeBinary, nullable=False) #Actual data, needed for Download
     rendered_data = db.Column(db.Text, nullable=False)#Data to render the pic in browser
 
 # initialize db
-def __init__(sundae, name, hasFlavors, description, Price, data):
+def __init__(sundae, name, has_flavors, description, price, data):
    sundae.name = name
-   sundae.hasFlavors = hasFlavors
+   sundae.has_flavors = has_flavors
    sundae.description = description
-   sundae.Price = Price
+   sundae.price = price
    sundae.data = data
 
 #cones
 class cones (db.Model):
     id = db.Column('cone_id', db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    hasFlavors = db.Column(db.String(10))
-    Price = db.Column(db.String(50))
+    has_flavors = db.Column(db.String(10))
+    price = db.Column(db.String(50))
     data = db.Column(db.LargeBinary, nullable=False) #Actual data, needed for Download
     rendered_data = db.Column(db.Text, nullable=False)#Data to render the pic in browser
 
     
 # initialize db
 
-def __init__(cone, name, hasFlavors, Price, data):
+def __init__(cone, name, has_flavors, price, data):
    cone.name = name
-   cone.hasFlavors = hasFlavors
-   cone.Price = Price
+   cone.has_flavors = has_flavors
+   cone.price = price
    cone.data = data
 
 
@@ -70,86 +71,86 @@ def __init__(cone, name, hasFlavors, Price, data):
 class bowls(db.Model):
     id = db.Column('bowl_id', db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    hasFlavors = db.Column(db.String(10))
-    Price = db.Column(db.String(50))
+    has_flavors = db.Column(db.String(10))
+    price = db.Column(db.String(50))
     data = db.Column(db.LargeBinary, nullable=False) #Actual data, needed for Download
     rendered_data = db.Column(db.Text, nullable=False)#Data to render the pic in browser
 
 # initialize db
-def __init__(bowl, name, hasFlavors, Price, data):
+def __init__(bowl, name, has_flavors, price, data):
    bowl.name = name
-   bowl.hasFlavors = hasFlavors
-   bowl.Price = Price
+   bowl.has_flavors = has_flavors
+   bowl.price = price
+   bowl.data = data
 
 
 #milkshakes
 class milkshakes (db.Model):
     id = db.Column('milkshake_id', db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    hasFlavors = db.Column(db.String(10))
-    Price = db.Column(db.String(50))
+    has_flavors = db.Column(db.String(10))
+    price = db.Column(db.String(50))
     data = db.Column(db.LargeBinary, nullable=False) #Actual data, needed for Download
     rendered_data = db.Column(db.Text, nullable=False)#Data to render the pic in browser
-  
-# initialize db
-def __init__(milkshake, name, hasFlavors, Price, data):
-   milkshake.name = name
-   milkshake.hasFlavors = hasFlavors
-   milkshake.Price = Price
-   milkshake.data = data
+    # initialize db
+    def __init__(milkshake, name, has_flavors, price, data):
+        milkshake.name = name
+        milkshake.has_flavors = has_flavors
+        milkshake.price = price
+        milkshake.data = data
 
 
 #smoothies table
 class smoothies (db.Model):
     id = db.Column('smoothie_id', db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    hasFlavors = db.Column(db.String(10))
-    Price = db.Column(db.String(50))
+    has_flavors = db.Column(db.String(10))
+    price = db.Column(db.String(50))
     data = db.Column(db.LargeBinary, nullable=False) #Actual data, needed for Download
     rendered_data = db.Column(db.Text, nullable=False)#Data to render the pic in browser
     
-# initialize db
-def __init__(smoothie, name, hasFlavors, Price, data):
-   smoothie.name = name
-   smoothie.hasFlavors = hasFlavors
-   smoothie.Price = Price
-   smoothie.data = data
+    # initialize db
+    def __init__(smoothie, name, has_flavors, price, data):
+        smoothie.name = name
+        smoothie.has_flavors = has_flavors
+        smoothie.price = price
+        smoothie.data = data
   
 
 #frappe table
 class frappes (db.Model):
     id = db.Column('frappe_id', db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    hasFlavors = db.Column(db.String(10))
-    Price = db.Column(db.String(50))
+    has_flavors = db.Column(db.String(10))
+    price = db.Column(db.String(50))
     data = db.Column(db.LargeBinary, nullable=False) #Actual data, needed for Download
     rendered_data = db.Column(db.Text, nullable=False)#Data to render the pic in browser
   
 # initialize db
-def __init__(frappe, name, hasFlavors, Price, data):
+def __init__(frappe, name, has_flavors, price, data):
    frappe.name = name
-   frappe.hasFlavors = hasFlavors
-   frappe.Price = Price
+   frappe.has_flavors = has_flavors
+   frappe.price = price
    frappe.data = data
 
 
 #snowcone
 class snowcones (db.Model):
-    id = db.Column('snowcone_id', db.Integer, primary_key=True)
+    id = db.Column('snowcone_id', db.String(50), primary_key=True)
     name = db.Column(db.String(50))
-    hasFlavors = db.Column(db.String(10))
-    Price = db.Column(db.String(50))
+    price = db.Column(db.String(50))
     description = db.Column(db.String(50))
     data = db.Column(db.LargeBinary, nullable=False) #Actual data, needed for Download
-    rendered_data = db.Column(db.Text, nullable=False)#Data to render the pic in browser
 
-# initialize db
-def __init__(snowcone, name, hasFlavors, Price, description, data):
-   snowcone.name = name
-   snowcone.hasFlavors = hasFlavors
-   snowcone.Price = Price
-   snowcone.description = description
-   snowcone.data = data 
+    # initialize db
+    def __init__(snowcone, id, name, price, description, data):
+        snowcone.id = id
+        snowcone.name = name
+        snowcone.price = price
+        snowcone.description = description
+        snowcone.data = data 
+    # def toJson(self):
+    #     return json.dumps(self, default=lambda o: o.__dict__)
 
 
 
@@ -176,10 +177,36 @@ def test_api():
                     'anothertest': 'anothersuccess'})
 
 
-@app.route('/api/getMilkshakes')
-def milkshakes_api():
-    milkshakes = get_all_milkshakes()
-    return jsonify([milkshake.to_json() for milkshake in milkshakes])
+@app.route('/api/getSnowcones')
+def get_snowcones_api():
+    results = snowcones.query.all()
+    result_list = []
+    for item in results:
+        result_list.append({
+            "id": item.id,
+            "name": item.name,
+            "description": item.description,
+            'price': item.price,
+            'imgSrc': item.data.decode('utf-8')
+        })
+    return json.dumps(result_list)
+
+@app.route('/api/test/resetDbData')
+def reset_db_data_api():
+    f = open(SITE_ROOT+'\defaultData.json')
+    data = json.load(f)
+    for i in data['stores']:
+        if i['name'] == 'snowConeType':
+            db.session.query(snowcones).delete()
+            db.session.commit()
+            for item in i['data']:
+                snowcone = snowcones(item['id'], item['name'], item['price'], item['description'], item['imgSrc'].encode('utf-8'))
+                db.session.add(snowcone)
+                db.session.commit()
+    return jsonify({'dbreset':'success'})
+
+    
+
 
 # @app.route('/smoothies.html')
 # def smoothies_page():
@@ -240,5 +267,6 @@ if __name__ == "__main__":
     #init_db.run()
     db.create_all()
     #create_app.run()
-    app.run(debug = True)
+    if 'liveconsole' not in gethostname():
+        app.run(debug = True)
    
