2a700780501679f3d23d7d84acd3a8953a45dddd
jacqeinz <jacquicv.19@gmail.com>
2023-04-30 21:59:57 -0600

    fix typo

--- Stage 4/frontend/snowcones.js
+++ Stage 4/frontend/snowcones.js
@@ -11,8 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
     setupTypes(db);
   };
 
-  const apiRequest = fetch('/api/getSnowcones');
-  apiRequest.then((response) => response.json())
+  const apiRequest = fetch("/api/getSnowcones");
+  apiRequest
+    .then((response) => response.json())
     .then((data) => console.log(data));
 
   // let confirmation = confirm("Do you wish to add this to the cart?");
@@ -21,50 +22,49 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function setupTypes(data) {
-      const productsDiv = document.getElementById("products");
-      for (type of data) {
-        let column = document.createElement("div");
-        if (type.hasFlavors) {
-          column.classList.add("w3-col", "l3", "s6", "goToFlavors");
-          column.setAttribute("onclick", "goToFlavors('" + type.id + "');");
-        } else {
-          column.classList.add("w3-col", "l3", "s6", "goToCart");
-          column.setAttribute("onclick", "addToCart('" + type.id + "');");
-        }
+  const productsDiv = document.getElementById("products");
+  for (type of data) {
+    let column = document.createElement("div");
+    if (type.hasFlavors) {
+      column.classList.add("w3-col", "l3", "s6", "goToFlavors");
+      column.setAttribute("onclick", "goToFlavors('" + type.id + "');");
+    } else {
+      column.classList.add("w3-col", "l3", "s6", "goToCart");
+      column.setAttribute("onclick", "addToCart('" + type.id + "');");
+    }
 
-        let containerDiv = document.createElement("div");
-        containerDiv.classList.add("w3-container");
-        column.append(containerDiv);
+    let containerDiv = document.createElement("div");
+    containerDiv.classList.add("w3-container");
+    column.append(containerDiv);
 
-        let imgDiv = document.createElement("div");
-        imgDiv.classList.add("w3-display-container");
-        containerDiv.append(imgDiv);
+    let imgDiv = document.createElement("div");
+    imgDiv.classList.add("w3-display-container");
+    containerDiv.append(imgDiv);
 
-        let img = document.createElement("img");
-        img.id = type.id + "_img";
-        img.width = "200";
-        img.src = type.imgSrc;
-        imgDiv.append(img);
+    let img = document.createElement("img");
+    img.id = type.id + "_img";
+    img.width = "200";
+    img.src = type.imgSrc;
+    imgDiv.append(img);
 
-        let nameP = document.createElement("p");
-        let nameContent = document.createTextNode(type.name);
-        let nameBr = document.createElement("br");
-        let priceB = document.createElement("b");
-        let priceContent = document.createTextNode("$" + type.price);
-        containerDiv.append(nameP);
-        nameP.append(nameContent);
-        nameP.append(nameBr);
-        nameP.append(priceB);
-        priceB.append(priceContent);
+    let nameP = document.createElement("p");
+    let nameContent = document.createTextNode(type.name);
+    let nameBr = document.createElement("br");
+    let priceB = document.createElement("b");
+    let priceContent = document.createTextNode("$" + type.price);
+    containerDiv.append(nameP);
+    nameP.append(nameContent);
+    nameP.append(nameBr);
+    nameP.append(priceB);
+    priceB.append(priceContent);
 
-        let figcaption = document.createElement("figcaption");
-        let captionContent = document.createTextNode(type.description);
-        containerDiv.append(figcaption);
-        figcaption.append(captionContent);
+    let figcaption = document.createElement("figcaption");
+    let captionContent = document.createTextNode(type.description);
+    containerDiv.append(figcaption);
+    figcaption.append(captionContent);
 
-        productsDiv.append(column);
-      }
-    };
+    productsDiv.append(column);
+  }
 }
 
 // const btns_flavors = document.querySelectorAll(".goToFlavors div");
@@ -79,5 +79,5 @@ function goToFlavors(type) {
 
 function addToCart(type) {
   console.log(type);
-  window.location.href="shoppingcart.html?type=snowcone&id="+type;
+  window.location.href = "shoppingcart.html?type=snowcone&id=" + type;
 }
