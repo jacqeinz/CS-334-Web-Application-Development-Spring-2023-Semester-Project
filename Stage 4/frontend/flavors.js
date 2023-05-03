72219b9030504f848d5a330ee13ee9010b9f8907
jacqeinz <jacquicv.19@gmail.com>
2023-05-03 11:52:47 -0600

    Fixed bad check on flavors page generation

--- Stage 4/frontend/flavors.js
+++ Stage 4/frontend/flavors.js
@@ -1,7 +1,6 @@
 setupDbConnection();
 
 document.addEventListener("DOMContentLoaded", function () {
-
   const apiRequest = fetch("/api/getFlavors");
   apiRequest
     .then((response) => response.json())
@@ -13,47 +12,51 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function setupTypes(data) {
-  console.log(data)
+  console.log(data);
   const productsDiv = document.getElementById("products");
   for (type of data) {
-    if (type.hasFlavors) {
-      let column = document.createElement("div");
-      column.classList.add("w3-col", "l3", "s6");
+    let column = document.createElement("div");
+    column.classList.add("w3-col", "l3", "s6");
 
-      let img = document.createElement("img");
-      img.id = type.id + "_img";
-      img.width = "200";
-      img.src = type.imgSrc;
+    let img = document.createElement("img");
+    img.id = type.id + "_img";
+    img.width = "200";
+    img.src = type.imgSrc;
 
-      let label = document.createElement("label");
-      label.class = "w3-container";
-      label.setAttribute("for", type.id);
-      let nameContent = document.createTextNode(type.name);
-      label.append(nameContent);
-      let input = document.createElement("input");
-      input.type = "checkbox";
-      input.id = type.id;
-      input.onclick = checkboxValidate;
+    let label = document.createElement("label");
+    label.class = "w3-container";
+    label.setAttribute("for", type.id);
+    let nameContent = document.createTextNode(type.name);
+    label.append(nameContent);
+    let input = document.createElement("input");
+    input.type = "checkbox";
+    input.id = type.id;
+    input.onclick = checkboxValidate;
 
-      column.append(img);
-      column.append(label);
-      column.append(input);
+    column.append(img);
+    column.append(label);
+    column.append(input);
 
-      productsDiv.append(column);
-    }
-  };
+    productsDiv.append(column);
+  }
 }
 
-function checkboxValidate(){
-  const checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
-  if(checkedBoxes.length >= 3){
-    const uncheckedBoxes = document.querySelectorAll('input[type="checkbox"]:not(:checked)');
-    for(box of uncheckedBoxes){
+function checkboxValidate() {
+  const checkedBoxes = document.querySelectorAll(
+    'input[type="checkbox"]:checked'
+  );
+  if (checkedBoxes.length >= 3) {
+    const uncheckedBoxes = document.querySelectorAll(
+      'input[type="checkbox"]:not(:checked)'
+    );
+    for (box of uncheckedBoxes) {
       box.disabled = true;
     }
-  } else{
-    const disabledBoxes = document.querySelectorAll('input[type="checkbox"]:disabled');
-    for(box of disabledBoxes){
+  } else {
+    const disabledBoxes = document.querySelectorAll(
+      'input[type="checkbox"]:disabled'
+    );
+    for (box of disabledBoxes) {
       box.disabled = false;
     }
   }
@@ -64,13 +67,13 @@ function addToCart() {
   const queryString = new URLSearchParams(window.location.search);
   let type, name, price;
 
-  if(!queryString.has("type") || !queryString.has("name")){
+  if (!queryString.has("type") || !queryString.has("name")) {
     //YELL AT USER
-    console.log("queryStringError")
-    console.log(queryString.get("type"))
-    console.log(queryString.get("id"))
-    console.log(queryString.get("name"))
-    console.log(queryString.get("price"))
+    console.log("queryStringError");
+    console.log(queryString.get("type"));
+    console.log(queryString.get("id"));
+    console.log(queryString.get("name"));
+    console.log(queryString.get("price"));
     return;
   }
   type = queryString.get("type");
@@ -78,19 +81,28 @@ function addToCart() {
   price = queryString.get("price");
   name = queryString.get("name");
 
-  const checkedFlavors = document.querySelectorAll('input[type="checkbox"]:checked');
+  const checkedFlavors = document.querySelectorAll(
+    'input[type="checkbox"]:checked'
+  );
   let flavorString = "";
-  if(checkedFlavors < 1){
+  if (checkedFlavors < 1) {
     //Yell at user
     return;
   }
-  for(i = 0; i < checkedFlavors.length; i++){
+  for (i = 0; i < checkedFlavors.length; i++) {
     flavorString += checkedFlavors[i].id;
-    if(i < checkedFlavors.length - 1){
+    if (i < checkedFlavors.length - 1) {
       flavorString += ",";
     }
   }
 
-
-  window.location.href = "shoppingcart.html?type="+ type +"&price="+ price +"&name=" + name + "&flavors=" + flavorString;
+  window.location.href =
+    "shoppingcart.html?type=" +
+    type +
+    "&price=" +
+    price +
+    "&name=" +
+    name +
+    "&flavors=" +
+    flavorString;
 }
