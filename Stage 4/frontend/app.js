be45e94882d93a7731a472bbd0b9ba21d8562c5f
jacqeinz <jacquicv.19@gmail.com>
2023-05-06 17:39:42 -0600

    correction to service worker

--- Stage 4/frontend/app.js
+++ Stage 4/frontend/app.js
@@ -1,8 +1,8 @@
 const registerServiceWorker = async () => {
     if ("serviceWorker" in navigator) {
       try {
-        const registration = await navigator.serviceWorker.register("/frontend/serviceWorker.js", {
-          scope: "/",
+        const registration = await navigator.serviceWorker.register("/serviceWorker.js", {
+          scope: "/index.html",
         });
         if (registration.installing) {
           console.log("Service worker installing");
