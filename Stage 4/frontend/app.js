1b5967f7bdd9e8b97eefdb3fcfd3233186f233fc
jacqeinz <jacquicv.19@gmail.com>
2023-05-06 18:24:57 -0600

    correct scope

--- Stage 4/frontend/app.js
+++ Stage 4/frontend/app.js
@@ -2,7 +2,7 @@ const registerServiceWorker = async () => {
     if ("serviceWorker" in navigator) {
       try {
         const registration = await navigator.serviceWorker.register("/serviceWorker.js", {
-          scope: "/",
+          scope: "/index.html",
         });
         if (registration.installing) {
           console.log("Service worker installing");
