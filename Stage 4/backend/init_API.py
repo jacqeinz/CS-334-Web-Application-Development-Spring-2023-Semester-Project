7a21e37a79f3ceb3dddf4f3fc5b96641f0fd98c7
TheEndlessVoid223 <111585938+TheEndlessVoid223@users.noreply.github.com>
2023-05-09 17:12:50 -0600

    sales

--- Stage 4/backend/init_API.py
+++ Stage 4/backend/init_API.py
@@ -402,6 +402,26 @@ def delete_order_api(order_id):
     db.session.delete(order)
     db.session.commit()
     return "Success"
+@app.route('/api/getSales')
+def get_sales_api():
+    results = Orders.query.all()
+    result_list = []
+    for item in results:
+        item_list = []
+        for i in item.items:
+            item_list.append({
+                'id': i.id,
+                'pname': i.pname,
+                'flavors': i.flavors,
+                'price': i.price
+            })
+        result_list.append({
+            # "id": item.id,
+            # "email": item.email,
+            # 'total': item.total,
+            'items': item_list
+        })
+    return json.dumps({'data':result_list})
 
 def loadInUsers():
     with open("defaultData.json", "r") as file:
