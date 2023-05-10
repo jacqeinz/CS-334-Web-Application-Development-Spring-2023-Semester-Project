92e431ceb54db4a51ec685cf29b0c7350a836bcf
jacqeinz <jacquicv.19@gmail.com>
2023-05-10 09:01:42 -0600

    attempting to fix PWA

--- Stage 4/frontend/common.js
+++ Stage 4/frontend/common.js
@@ -1,9 +1,8 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-    console.log('ServiceWorker registration successful with scope:',  registration.scope);
-  }).catch(function(error) {
-    console.log('ServiceWorker registration failed:', error);
-  });
+if ('serviceWorker' in navigator) {
+ window.addEventListener('load', function(){
+  navigator.serviceWorker.register('service-worker.js');
+ });
+
 }
 async function setupDbConnection() {
   let json = await fetch("defaultData.json").then((response) =>
