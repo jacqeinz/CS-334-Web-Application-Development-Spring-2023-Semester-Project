e26708b3f62dd057e9700bf15ea5c0507065f97d
jacqeinz <jacquicv.19@gmail.com>
2023-05-09 21:36:46 -0600

    correction manifest and service worker

--- Stage 4/frontend/common.js
+++ Stage 4/frontend/common.js
@@ -1,8 +1,10 @@
-if (typeof navigator.serviceWorker !== 'undefined') {
-  navigator.serviceWorker.register('service-worker.js')
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    console.log('ServiceWorker registration successful with scope:',  registration.scope);
+  }).catch(function(error) {
+    console.log('ServiceWorker registration failed:', error);
+  });
 }
-
-
 async function setupDbConnection() {
   let json = await fetch("defaultData.json").then((response) =>
     response.json()
