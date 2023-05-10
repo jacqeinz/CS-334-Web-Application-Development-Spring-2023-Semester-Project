201b6f066888969d26332eb3969f3b3f0c36d4d7
jacqeinz <jacquicv.19@gmail.com>
2023-05-09 20:12:07 -0600

    Fixed login page

--- Stage 4/frontend/loginValid.js
+++ Stage 4/frontend/loginValid.js
@@ -1,44 +1,40 @@
 setupDbConnection();
 
 document.addEventListener("DOMContentLoaded", function () {
-  const apiRequest = fetch("https://jacqiec.pythonanywhere.com/api/managerPortalLogin");
-  apiRequest
-    .then((response) => response.json())
-    .then((data) => checkDbID(data.data));
+
 
 });
 
-function checkDbID(data) {
-  console.log(data)
+function checkDbID() {
 
-    let usernameInput = document.getElementById("username");
-    let passwordInput = document.getElementById("password");
+  let usernameInput = document.getElementById("username");
+  let passwordInput = document.getElementById("password");
 
 
-    let password = passwordInput.value.trim();
-    let username = usernameInput.value.trim();
-    
+  let password = passwordInput.value.trim();
+  let username = usernameInput.value.trim();
 
-    fetch("/api/managementPortalLogin/" + id, {
-      method: "POST",
-      body: JSON.stringify({
-        username: username,
-        password: password
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
+
+  fetch("/api/managementPortalLogin/" + id, {
+    method: "POST",
+    body: JSON.stringify({
+      username: username,
+      password: password
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    }
+  })
+
+    .then((response) => response.text())
+    .then((text) => {
+      if (text == -1) {
+        alert("Username or Password is incorrect, please try again!");
+        return;
       }
-    })
-   
-        .then((response) => response.text())
-        .then((text) => {
-          if(text == -1){
-            alert("Username or Password is incorrect, please try again!");
-            return;
-          }
-          console.log(text);
-          sessionStorage.setItem("userId", text);
-          window.location.href = "management.html";
-        });
+      console.log(text);
+      sessionStorage.setItem("userId", text);
+      window.location.href = "management.html";
+    });
 };
-      
+
